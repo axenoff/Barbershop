51c7907017ea68fc0540a7f1d56ca02726d82f8b
axenoff <axenoff@gmail.com>
2017-02-08 22:49:00 +0300

    change mail

--- app.rb
+++ app.rb
@@ -68,14 +68,14 @@ post '/contacts' do
     f.close
 	
 	Pony.mail({
-  :to => 'axenoff@gmail.com',
+  :to => 'axenoffruby@gmail.com',
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
     :user_name            => 'axenoffruby',
-    :password             => '2140449ruby',
+    :password             => 'qwertyruby',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
 
